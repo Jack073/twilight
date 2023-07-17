c131a89a8925844cdb56093c4766cad1bb3b8b7f
Tim Vilgot Mikael Fredenberg <vilgot.fredenberg@proton.me>
2023-07-17 19:09:10 +0200

    cleanup tests

--- twilight-gateway-queue/tests/common/mod.rs
+++ twilight-gateway-queue/tests/common/mod.rs
@@ -1,18 +1,14 @@
-use std::{
-    future::{poll_fn, Future},
-    pin::Pin,
-};
 use tokio::time::{advance, Duration, Instant};
 use twilight_gateway_queue::{Queue, IDENTIFY_DELAY, LIMIT_PERIOD};
 
 pub async fn same_id_is_serial(queue: impl Queue) {
     let now = Instant::now();
 
-    let mut t1 = queue.enqueue(0);
-    let mut t2 = queue.enqueue(0);
+    let t1 = queue.enqueue(0);
+    let t2 = queue.enqueue(0);
 
-    _ = poll_fn(|cx| Pin::new(&mut t1).poll(cx)).await;
-    _ = poll_fn(|cx| Pin::new(&mut t2).poll(cx)).await;
+    _ = t1.await;
+    _ = t2.await;
 
     assert!(now.elapsed() >= IDENTIFY_DELAY, "ran concurrently");
 }
@@ -22,25 +18,25 @@ pub async fn different_id_is_parallel(queue: impl Queue) {
     let now = Instant::now();
 
     let mut t1 = queue.enqueue(1);
-    let mut t2 = queue.enqueue(0);
+    let t2 = queue.enqueue(0);
 
     tokio::select! {
         biased;
-        _ = poll_fn(|cx| Pin::new(&mut t1).poll(cx)) => panic!("not started in order"),
-        _ = poll_fn(|cx| Pin::new(&mut t2).poll(cx)) => {
-            _ = poll_fn(|cx| Pin::new(&mut t1).poll(cx)).await;
+        _ = &mut t1 => panic!("not started in order"),
+        _ = t2 => {
+            _ = t1.await;
             assert!(now.elapsed() < IDENTIFY_DELAY, "ran serially");
         }
     }
 }
 
-/// Requires a queue with `total` >= 1.
+/// Requires a queue with `remaining` of 0.
 pub async fn reset_after_refills(queue: impl Queue, reset_after: Duration) {
     let now = Instant::now();
 
-    let mut t1 = queue.enqueue(0);
+    let t1 = queue.enqueue(0);
 
-    _ = poll_fn(|cx| Pin::new(&mut t1).poll(cx)).await;
+    _ = t1.await;
 
     assert!(
         (now.elapsed().as_secs_f64() - reset_after.as_secs_f64()).abs() <= 1e-2,
@@ -52,17 +48,17 @@ pub async fn reset_after_refills(queue: impl Queue, reset_after: Duration) {
 pub async fn reset_after_started(queue: impl Queue) {
     advance(LIMIT_PERIOD / 2).await;
 
-    let mut t1 = queue.enqueue(0);
-    let mut t2 = queue.enqueue(0);
+    let t1 = queue.enqueue(0);
+    let t2 = queue.enqueue(0);
 
-    _ = poll_fn(|cx| Pin::new(&mut t1).poll(cx)).await;
+    _ = t1.await;
 
     let now = Instant::now();
 
-    _ = poll_fn(|cx| Pin::new(&mut t2).poll(cx)).await;
+    _ = t2.await;
 
     assert!(
-        (dbg!(now.elapsed().as_secs_f64()) - dbg!(LIMIT_PERIOD.as_secs_f64())).abs() <= 1e-2,
+        (now.elapsed().as_secs_f64() - LIMIT_PERIOD.as_secs_f64()).abs() <= 1e-2,
         "queue misstimed remaining refill"
     );
 }
@@ -71,18 +67,18 @@ pub async fn reset_after_started(queue: impl Queue) {
 pub async fn multi_bucket(queue: impl Queue) {
     let now = Instant::now();
 
-    let mut t1 = queue.enqueue(0);
-    let mut t2 = queue.enqueue(1);
-    let mut t3 = queue.enqueue(3);
-    let mut t4 = queue.enqueue(3);
+    let t1 = queue.enqueue(0);
+    let t2 = queue.enqueue(1);
+    let t3 = queue.enqueue(3);
+    let t4 = queue.enqueue(3);
 
-    _ = poll_fn(|cx| Pin::new(&mut t1).poll(cx)).await;
-    _ = poll_fn(|cx| Pin::new(&mut t2).poll(cx)).await;
-    _ = poll_fn(|cx| Pin::new(&mut t3).poll(cx)).await;
+    _ = t1.await;
+    _ = t2.await;
+    _ = t3.await;
 
     assert!(now.elapsed() < IDENTIFY_DELAY, "ran serially");
 
-    _ = poll_fn(|cx| Pin::new(&mut t4).poll(cx)).await;
+    _ = t4.await;
 
     assert!(now.elapsed() >= IDENTIFY_DELAY, "ran concurrently");
 }
--- twilight-gateway-queue/tests/in_memory.rs
+++ twilight-gateway-queue/tests/in_memory.rs
@@ -1,58 +1,53 @@
 mod common;
 
-use common::{different_id_is_parallel, multi_bucket, reset_after_refills, same_id_is_serial};
-use std::{
-    future::{poll_fn, Future},
-    pin::Pin,
-};
-use tokio::time::{advance, Duration, Instant};
+use common::*;
+use tokio::time::{Duration, Instant};
 use twilight_gateway_queue::{InMemoryQueue, Queue, IDENTIFY_DELAY};
 
 #[tokio::test]
 async fn disabled_is_instant() {
-    let queue = InMemoryQueue::new(0, 1000, Duration::from_secs(60 * 60 * 24), 1000);
+    let queue = InMemoryQueue::new(0, 0, Duration::ZERO, 0);
     let now = Instant::now();
 
-    let mut t1 = queue.enqueue(0);
-    let mut t2 = queue.enqueue(0);
+    let t1 = queue.enqueue(0);
+    let t2 = queue.enqueue(0);
 
-    _ = poll_fn(|cx| Pin::new(&mut t1).poll(cx)).await;
-    _ = poll_fn(|cx| Pin::new(&mut t2).poll(cx)).await;
+    _ = t1.await;
+    _ = t2.await;
 
     assert!(now.elapsed() < IDENTIFY_DELAY, "did not run instantly");
 }
 
 #[tokio::test]
 async fn update_fills_bucket() {
-    let queue = InMemoryQueue::new(1, 1000, Duration::from_secs(60 * 60 * 24), 1000);
+    let queue = InMemoryQueue::new(1, 10, Duration::from_secs(60), 10);
     let now = Instant::now();
 
     // Background task not run due to single-threaded runtime.
-    let mut t1 = queue.enqueue(0);
-    let mut t2 = queue.enqueue(1);
-    queue.update(2, 1000, Duration::from_secs(60 * 60 * 24), 1000);
+    let t1 = queue.enqueue(0);
+    let t2 = queue.enqueue(1);
+    queue.update(2, 10, Duration::from_secs(60), 10);
 
-    _ = poll_fn(|cx| Pin::new(&mut t1).poll(cx)).await;
-    _ = poll_fn(|cx| Pin::new(&mut t2).poll(cx)).await;
+    _ = t1.await;
+    _ = t2.await;
 
     assert!(now.elapsed() < IDENTIFY_DELAY, "ran serially");
 }
 
 #[tokio::test(start_paused = true)]
 async fn integration() {
-    let queue = InMemoryQueue::new(1, 1000, Duration::from_secs(60 * 60 * 24), 1000);
+    let mut queue = InMemoryQueue::new(1, 1000, Duration::ZERO, 1000);
+    same_id_is_serial(&queue).await;
 
-    same_id_is_serial(queue.clone()).await;
+    queue = InMemoryQueue::new(2, 1000, Duration::ZERO, 1000);
+    different_id_is_parallel(&queue).await;
 
-    advance(IDENTIFY_DELAY).await;
-    queue.update(2, 1000, Duration::from_secs(60 * 60 * 24), 1000);
-    different_id_is_parallel(queue.clone()).await;
+    queue = InMemoryQueue::new(1, 0, Duration::from_secs(60), 1);
+    reset_after_refills(&queue, Duration::from_secs(60)).await;
 
-    advance(IDENTIFY_DELAY).await;
-    queue.update(1, 0, Duration::from_secs(60), 1);
-    reset_after_refills(queue.clone(), Duration::from_secs(60)).await;
+    queue = InMemoryQueue::new(1, 1, Duration::ZERO, 1);
+    reset_after_started(&queue).await;
 
-    advance(IDENTIFY_DELAY).await;
-    queue.update(4, 1000, Duration::from_secs(60 * 60 * 24), 1000);
+    queue = InMemoryQueue::new(4, 1000, Duration::ZERO, 1000);
     multi_bucket(queue).await;
 }
